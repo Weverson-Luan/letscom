b3c884f68bc34949bf7514ae43b31bd6e3cd13c9
Weverson-Luan <wltech2022@gmail.com>
2024-10-13 14:34:14 -0300

    deploy 🚀 config de deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/letscom/", // Garante que os assets tenham caminho relativo
+  base: "/letscom", // Garante que os assets tenham caminho relativo
 });
